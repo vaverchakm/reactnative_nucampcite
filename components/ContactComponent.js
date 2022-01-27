13e0fc6e6cd83cff7b57c33ab3304b1345a247cd
Maksym Vaverchak <vaverchakm@gmail.com>
2022-01-27 18:39:24 -0500

    Navigation Icons and Custom Side Drawer

---
+++ components/ContactComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { ScrollView, Text } from 'react-native';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+import { Card } from 'react-native-elements';
+
+
+
+
+class Contact extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+            promotions: PROMOTIONS,
+            partners: PARTNERS
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+
+    render() {
+        return (
+            <ScrollView>
+               <Card title="Contact Information" wrapperStyle={{margin: 20}}>
+                   <Text>1 Nucamp Way</Text>
+                   <Text>Seattle, WA 98001</Text>
+                   <Text style={{marginBottom: 10}}>U.S.A</Text>
+
+                   <Text>
+                       Phone: 1-206-555-1234
+                       Email: campsites@nucamp.co
+                   </Text>
+               </Card>
+            </ScrollView>
+        );
+    }
+}
+
+export default Contact;
